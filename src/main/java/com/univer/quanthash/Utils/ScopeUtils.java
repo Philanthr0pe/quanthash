47cc60bf01c8c07775b9bbe4d13144d9885814a1
Vlad <ivladislav969@gmail.com>
2017-04-20 11:34:12 +0300

    start bees alg

---
+++ src/main/java/com/univer/quanthash/Utils/ScopeUtils.java
@@ -0,0 +1,9 @@
+package com.univer.quanthash.Utils;
+
+/**
+ * Created by ASUS-PC on 19.04.2017.
+ */
+public class ScopeUtils {
+    
+
+}
--- src/main/java/com/univer/quanthash/genetic/swarmOfBees/BeesAlgorithm.java
+++ src/main/java/com/univer/quanthash/genetic/swarmOfBees/BeesAlgorithm.java
@@ -1,11 +1,50 @@
 package com.univer.quanthash.genetic.swarmOfBees;
 
+import com.univer.quanthash.DeltaFunction;
+import com.univer.quanthash.models.DeltaModel;
+import com.univer.quanthash.random.RandomAlgorithm;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 /**
  * Created by Vladislav on 14-Apr-17.
  */
 public class BeesAlgorithm {
 
+    private int startCountOfAreas;
     private int countOfBees;
+    private int countOfBeesForBest;
+    private int countOfBeesForWorst;
+    private int countOfBestAreas;
+    private int countOfWorstAreas;
+    private int sizeOfArea;
+    private DeltaFunction deltaFunction;
+
+    public BeesAlgorithm(int countOfAreas) {
+        startCountOfAreas = countOfAreas;
+        countOfBees = 10;
+        countOfBeesForBest = 5;
+        countOfBeesForWorst = 2;
+        countOfBestAreas = 2;
+        countOfWorstAreas = 3;
+        sizeOfArea = 10;
+        deltaFunction = new DeltaFunction();
+    }
+
+    private void function(int q, int d) {
+        Set<int[]> ints = new RandomAlgorithm().generateRandomArrs(q, d, this.startCountOfAreas);
+        Set<DeltaModel> deltaModels = deltaFunction.deltaFunctionForSet(ints);
+        List<DeltaModel> collectOfDeltaModels = deltaModels.stream()
+                .sorted((o1, o2) -> Double.compare(o1.getDelta(), o2.getDelta()))
+                .collect(Collectors.toList());
+
+        for (int i = 0; i < countOfBestAreas; i++) {
+            collectOfDeltaModels.get(i);
+        }
+    }
+    
 
 
 }
